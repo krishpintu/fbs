55fe17eb77a63c8e9bfeabe0e76b04c6137db317
Krishna Singh (TTL) <krishnas.ttl@tatamotors.com>
2022-06-22 14:48:57 +0530

    init

---
+++ src/main/java/com/easydeal/fbs/FbsApplication.java
@@ -0,0 +1,13 @@
+package com.easydeal.fbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easydeal/fbs/FbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easydeal.fbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
